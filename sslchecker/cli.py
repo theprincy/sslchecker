3207161fc992f9d8922d78d2c3ff3fc916611bae
raccoony <raccoonyy@gmail.com>
2015-10-14 14:59:47 +0900

    필요 없는 부분 삭제

--- sslchecker/cli.py
+++ sslchecker/cli.py
@@ -12,14 +12,12 @@ from .lib import get_sslcert
 @click.argument('domain')
 def cli(domain):
     """A simple SSL certificates validator on your command line."""
-    try:
-        sslcert = get_sslcert(domain)
-    except:
-        click.secho("#################", fg='red', bold=True)
-        click.secho("# SSL unsecured #", fg='red', bold=True)
-        click.secho("#################", fg='red', bold=True)
-        click.echo("No SSL certificates were found on {}.".format(domain))
-        return False
+    sslcert = get_sslcert(domain)
+    # click.secho("#################", fg='red', bold=True)
+    # click.secho("# SSL unsecured #", fg='red', bold=True)
+    # click.secho("#################", fg='red', bold=True)
+    # click.echo("No SSL certificates were found on {}.".format(domain))
+    # sys.exit()
 
     expire_in = _expire_in(sslcert)
     secured = _secured(sslcert)
