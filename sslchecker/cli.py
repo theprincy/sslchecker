c024d9c3db491ddd0c9b367aed65dbff56abe7d9
raccoony <raccoonyy@gmail.com>
2015-10-14 17:51:49 +0900

    cert 출력 부분을 render_cert 함수로 옮김

--- sslchecker/cli.py
+++ sslchecker/cli.py
@@ -2,11 +2,9 @@
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
 import click
-import datetime
-import pycountry
 import sys
 
-from .lib import get_sslcert
+from .lib import get_sslcert, render_cert
 
 
 @click.command()
@@ -22,69 +20,4 @@ def cli(domain):
         click.echo("No SSL certificates were found on {}.".format(domain))
         sys.exit()
 
-    expire_in = _expire_in(sslcert)
-    secured = _secured(sslcert)
-    warning = _warning(sslcert)
-
-    secure_text = '# SSL secured #'
-    append_secure_text = '###############'
-
-    if secured and not warning:
-        color = 'green'
-    elif secured and warning:
-        color = 'yellow'
-    else:
-        color = 'red'
-        secure_text = '# SSL unsecured #'
-        append_secure_text = '#################'
-
-    click.secho("{}".format(append_secure_text), fg=color, bold=True)
-    click.secho("{}".format(secure_text), fg=color, bold=True)
-    click.secho("{}\n".format(append_secure_text), fg=color, bold=True)
-
-    click.echo("Common name: {0.cn}".format(sslcert))
-    click.echo("Valid from: {0.notbefore}".format(sslcert))
-
-    if secured:
-        click.echo("Expiration date: {0.notafter}".format(sslcert))
-        click.echo("\tExpire in {0} days.\n".format(expire_in))
-    else:
-        click.secho("Expiration date: {0.notafter}".format(sslcert), fg='red')
-
-    click.echo("Signature Algorithm: {0.keyinfo[0]}".format(sslcert))
-    click.echo("Key length: {0.keyinfo[1]}".format(sslcert))
-    click.echo("Issued by:".format(sslcert))
-    for issuer in sslcert.issuer:
-        cat_name = issuer[0]
-        cat_value = issuer[1]
-        cat = ''
-        if cat_name == 'O':
-            cat = 'Organization'
-        elif cat_name == 'C':
-            cat = 'Country'
-            cat_value = pycountry.countries.get(alpha2=cat_value).name
-        elif cat_name == 'OU':
-            cat = 'Organization unit'
-        elif cat_name == 'CN':
-            cat = 'Common name'
-        elif cat_name == 'ST':
-            cat = 'State'
-        elif cat_name == 'L':
-            cat = 'Locality'
-
-        click.echo("\t{0}: {1}".format(cat, cat_value))
-
-    return None
-
-
-def _warning(sslcert):
-    return (0 < _expire_in(sslcert)) and (_expire_in(sslcert) < 30)
-
-
-def _secured(sslcert):
-    return not sslcert.has_expired
-
-
-def _expire_in(sslcert):
-    d = sslcert.notafter - datetime.datetime.now()
-    return d.days
+    render_cert(sslcert)
--- sslchecker/lib.py
+++ sslchecker/lib.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import (unicode_literals)
 from .sslcert import SSLCert
+import click
+import datetime
 import OpenSSL
+import pycountry
 import socket
 import ssl
 try:
@@ -24,3 +27,73 @@ def get_sslcert(domain):
         raise err
     else:
         return SSLCert(x509)
+
+
+def render_cert(sslcert):
+    expire_in = _expire_in(sslcert)
+    secured = _secured(sslcert)
+    warning = _warning(sslcert)
+
+    secure_text = '# SSL unsecured #'
+    append_secure_text = '#################'
+
+    if secured and not warning:
+        color = 'green'
+        secure_text = '# SSL secured #'
+        append_secure_text = '###############'
+    elif secured and warning:
+        color = 'yellow'
+    else:
+        color = 'red'
+
+    click.secho("{}".format(append_secure_text), fg=color, bold=True)
+    click.secho("{}".format(secure_text), fg=color, bold=True)
+    click.secho("{}\n".format(append_secure_text), fg=color, bold=True)
+
+    click.echo("Common name: {0.cn}".format(sslcert))
+    click.echo("Valid from: {0.notbefore}".format(sslcert))
+
+    if secured:
+        click.echo("Expiration date: {0.notafter}".format(sslcert))
+        click.echo("\tExpire in {0} days.\n".format(expire_in))
+    else:
+        click.secho("Expiration date: {0.notafter}".format(sslcert), fg='red')
+
+    click.echo("Signature Algorithm: {0.keyinfo[0]}".format(sslcert))
+    click.echo("Key length: {0.keyinfo[1]}".format(sslcert))
+    click.echo("Issued by:".format(sslcert))
+    for issuer in sslcert.issuer:
+        cat_name = issuer[0]
+        cat_value = issuer[1]
+
+        cat = ''
+        if cat_name == 'O':
+            cat = 'Organization'
+        elif cat_name == 'C':
+            cat = 'Country'
+            cat_value = pycountry.countries.get(alpha2=cat_value).name
+        elif cat_name == 'OU':
+            cat = 'Organization unit'
+        elif cat_name == 'CN':
+            cat = 'Common name'
+        elif cat_name == 'ST':
+            cat = 'State'
+        elif cat_name == 'L':
+            cat = 'Locality'
+
+        click.echo("\t{0}: {1}".format(cat, cat_value))
+
+    return None
+
+
+def _warning(sslcert):
+    return (0 < _expire_in(sslcert)) and (_expire_in(sslcert) < 30)
+
+
+def _secured(sslcert):
+    return not sslcert.has_expired
+
+
+def _expire_in(sslcert):
+    d = sslcert.notafter - datetime.datetime.now()
+    return d.days
