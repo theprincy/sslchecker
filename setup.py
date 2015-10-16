b26d4cf495f1333a271107e51465924867611e8b
raccoony <raccoonyy@gmail.com>
2015-10-16 21:05:34 +0900

    version 0.1.5

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ extras_require = {
 
 setup(
     name='sslchecker',
-    version='0.1.4',
+    version='0.1.5',
     url='https://github.com/raccoonyy/sslchecker',
     author='Seungho Kim',
     author_email='raccoonyy@gmail.com',
